9cd2668090aa8b6c6f42dcc9d90a93e1549d7560
Marcel <marcelfelipe10@gmail.com>
2020-03-16 17:58:10 -0300

    Atualizando a Action Post Message

---
+++ actions/message.py
@@ -0,0 +1,41 @@
+import requests
+import six
+
+from six.moves.urllib.parse import urlencode
+from six.moves.urllib import parse as urlparse
+urljoin = urlparse.urljoin
+
+from st2common.runners.base_action import Action
+
+BASE_URL = 'https://harold.indrabrasil.com.br/api-chat/message'
+
+
+class SlackAction(Action):
+
+    def run(self, **kwargs):
+        return self.request(kwargs)
+
+    def request(self, params):
+        question = params['question']
+        channel = params['channel']
+        user = params['user']
+        bot = params['bot']
+
+        headers = {}
+        headers['Content-Type'] = 'application/json'
+
+        data = {}
+        data['channelType'] = channel
+        data['question'] = question
+        data['botId'] = bot
+        data['user'] = user
+
+        response = requests.post(url=BASE_URL, headers=headers, data=data)
+        results = response.json()
+
+        if not results['ok']:
+            failure_reason = ('Failed to perform action %s: %s \(status code: %s)' % (end_point, response.text, response.status_code))
+            self.logger.exception(failure_reason)
+            raise Exception(failure_reason)
+
+        return results
