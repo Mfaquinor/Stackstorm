0fbfcbb59022c596c143509ad08d10548b6bb0f1
Marcel <marcelfelipe10@gmail.com>
2020-03-18 12:13:05 -0300

    Json Parser do Request HTTP

--- actions/message.py
+++ actions/message.py
@@ -1,4 +1,5 @@
 import requests
+import json
 from st2common.runners.base_action import Action
 
 BASE_URL = 'https://harold.indrabrasil.com.br/api-chat/message'
@@ -17,10 +18,16 @@ class SlackAction(Action):
         headers = {}
         headers['Content-Type'] = 'application/json'
 
-        data = {}
-        data['channelType'] = channel
-        data['question'] = question
-        data['botId'] = bot
-        data['user'] = user
+        self.logger.info(bot)
+        self.logger.info(user)
+        self.logger.info(channel)
+        self.logger.info(question)
 
-        return requests.post(url=BASE_URL, headers=headers, data=data)
+        payload = {
+            'question': question,
+            'user': user
+            'botId': bot
+            'channelType': channel
+        }
+
+        return requests.post(url=BASE_URL, headers=headers, data=json.dumps(payload))
