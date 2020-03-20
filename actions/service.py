3f15b1fd3a28ebf5a9d2a62cb17f7db6d06fabf7
Marcel <marcelfelipe10@gmail.com>
2020-03-20 20:05:39 -0300

    Controlador Sobre o Responsável pelo Atendimento

---
+++ actions/service.py
@@ -0,0 +1,40 @@
+import requests
+import json
+
+from st2common.runners.base_action import Action
+
+HAROLD = 'https://localhost/api/v1/webhooks/harold/message'
+CONNECTACHAT = 'https://localhost/api/v1/webhooks/oop/message'
+
+BASE_URL = ''
+
+class SlackAction(Action):
+
+    def run(self, **kwargs):
+        params = kwargs
+
+        username = params['username']
+        message = params['message']
+        userid = params['userid']
+
+        key = 'attendance:' + userid
+        attendance = self.action_service.get_value(key)
+
+        self.logger.info(attendance)
+
+        if(attendance is None):
+            BASE_URL = HAROLD
+        else:
+            BASE_URL = CONNECTACHAT
+
+
+        headers = {}
+        headers['Content-Type'] = 'application/json'
+
+        payload = {
+            'message': message,
+            'username': username,
+            'userid': userid
+        }
+
+        return requests.post(url=BASE_URL, headers=headers, data=json.dumps(payload))
